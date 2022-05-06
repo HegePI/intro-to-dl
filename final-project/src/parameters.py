292c45aab64c7124a730042e269e462eb371961c
Heikki Pulli <heikki.pulli@helsinki.fi>
2022-05-06 12:24:22 +0300

    comments on Parameters class

--- final-project/src/parameters.py
+++ final-project/src/parameters.py
@@ -2,6 +2,8 @@ import json
 
 
 class Parameters:
+    """Class to load and access params in hyperparameters.json, defaults to "base" mode"""
+
     def __init__(self, mode="base"):
         self.mode = mode
 
@@ -9,10 +11,13 @@ class Parameters:
             self.params = json.loads(file.read())
 
     def get(self, attr):
+        """Get a named attribute"""
         return self.params[self.mode][attr]
 
     def set_mode(self, mode):
+        """Set mode defined in hyperparameters.json"""
         self.mode = mode
 
     def get_mode(self):
+        """Get mode"""
         return self.mode
