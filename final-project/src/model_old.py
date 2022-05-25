8c6bf5e05a78a6eea63e56a929d1bc3dbeb415fe
Pikipum <36789386+Pikipum@users.noreply.github.com>
2022-05-25 20:03:36 +0300

    old model

---
+++ final-project/src/model_old.py
@@ -0,0 +1,34 @@
+import torch
+
+
+class Model(torch.nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        embedding_dim: int,
+        lstm_hidden_dim: int,
+        num_classes: int,
+    ):
+        super(Model, self).__init__()
+        self.embedding = torch.nn.Embedding(
+            num_embeddings=vocab_size, embedding_dim=embedding_dim, padding_idx=0
+        )
+        self.lstm_layer = torch.nn.LSTM(embedding_dim, lstm_hidden_dim)
+        self.linear = torch.nn.Linear(
+            in_features=lstm_hidden_dim, out_features=num_classes
+        )
+
+    def forward(self, seqs, seqs_lengths):
+
+        embedding_output = self.embedding(seqs)
+        packed_embedded = torch.nn.utils.rnn.pack_padded_sequence(
+            embedding_output, seqs_lengths.cpu()
+        )
+
+        lstm_output, (hidden_state, _) = self.lstm_layer(packed_embedded)
+        ouput, _ = torch.nn.utils.rnn.pad_packed_sequence(lstm_output)
+
+        linear_output = self.linear(hidden_state[-1])
+
+        out = torch.sigmoid(linear_output)
+        return out
