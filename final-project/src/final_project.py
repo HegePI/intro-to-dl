710bd3b5dc95dac54deca74bfc7e8f4716cbabd2
markus <markus.tervonen@hotmail.com>
2022-05-26 14:00:08 +0300

    change to criterion

--- final-project/src/final_project.py
+++ final-project/src/final_project.py
@@ -114,9 +114,10 @@ if __name__ == "__main__":
 
     # BCEWithLogitsLoss, when models last layer is not sigmoid
     # https://pytorch.org/docs/stable/generated/torch.nn.BCEWithLogitsLoss.html?highlight=bceloss
-    criterion = torch.nn.BCEWithLogitsLoss(
-        pos_weight=torch.ones(params.get("num_classes"))
-    )
+    #criterion = torch.nn.BCEWithLogitsLoss(
+    #    pos_weight=torch.ones(params.get("num_classes"))
+    #)
+    criterion = torch.nn.BCEWithLogitsLoss(pos_weight=(torch.ones([128]) * 3).to(device))
     # criterion = torch.nn.BCEWithLogitsLoss()
 
     optimizer = get_optimizer(
